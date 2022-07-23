b66c9fc1f564d3424d4fd7c6a399d37f2e462b77
dominggo1999 <arnolddominggo19@gmail.com>
2022-07-23 11:25:11 +0700

    add : import color-schemes.css on main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './App';
-import GlobalStyles from './styles/GlobalStyles';
+import App from './App.jsx';
+import GlobalStyles from './styles/GlobalStyles.jsx';
+import './color-schemes/color-schemes.css';
 
 ReactDOM.render(
   <React.StrictMode>
